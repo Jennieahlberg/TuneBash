b1ab0e04e12797a2845efa15e664316d1c931529
Gabriela <g.saavedra.letelier@gmail.com>
2018-09-03 10:15:29 +0200

    initializing Spring

---
+++ src/main/java/com/example/demo/TunebashApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunebashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TunebashApplication.class, args);
+    }
+}
