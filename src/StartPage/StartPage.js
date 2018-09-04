af33cf76272353967e3a0887a1eade98c5c63009
katarinasandart <katarina.sandart@hotmail.com>
2018-09-04 17:33:03 +0200

    Change spotify login

--- src/StartPage/StartPage.js
+++ src/StartPage/StartPage.js
@@ -4,6 +4,7 @@ import "./StartPage.css";
 
 
 const startPage = props => {
+  
   return (
     <div>
       <div className="logo">
@@ -22,6 +23,11 @@ const startPage = props => {
       <div>
         <p className="logoName">TUNEBASH</p>
       </div>
+      <div>
+        <a href="/login" className="btn btn-primary">
+          Log in with Spotify
+        </a>
+      </div>
       <div className="codeInput">
         <input
           id="codeInput"
@@ -37,7 +43,8 @@ const startPage = props => {
         <button id="newGameButton" onClick={props.onClickNew}>
           Generera nytt spel
         </button>
-      </div>
+        
+      </div>  
     </div>
   );
 };
