49890b31f03c591c73ab2720e8bdff76b4c35aa6
Gustaf Matsson <gustafmatsson@gmail.com>
2018-09-04 15:36:22 +0200

    Made different getmapping methods for adding/getting data from db. Made Excelreader class that adds questions to db from and excel file. Added a cool banner.txt

--- src/main/java/com/example/demo/QuestionsRepository.java
+++ src/main/java/com/example/demo/QuestionsRepository.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionsRepository  extends JpaRepository<Questions, Integer> {
+    Questions getById(int random);
 }
