621eab9926a007c84e59c05ae81d1895cddabaf8
Katarina Käll <katarina_kall86@hotmail.com>
2018-09-06 14:03:56 +0200

    Changed stuff in question.js

--- src/Question/Question.js
+++ src/Question/Question.js
@@ -1,26 +1,41 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./Question.css";
 
 class Question extends Component {
-  render() {
-    const Question = this.props.question;
-    return (
-      <div className="Question">
+    state = {
+        answer: "japps",
+        question: "sugen på blåbär?"
+    };
+    // constructor(props){
+    //     super(props)
+    // }
 
-        <div id="Question_question">
-          <div className="Question__cat">{question.varugrupp}</div>
-          <h2>{question.question}</h2>
-          <div className="Question_answer">{question.answers}</div>
-          <h3>{question.answer1}</h3>
-          <h3>{question.answer2}</h3>
-          <h3>{question.answer3}</h3>
-          <h3>{question.answer4}</h3>
-          
-        </div>
-      </div>
-    );
-  }
-}
+    submitAnswer = () => {
+    }
+
+    render() {
+        const answer = this.state.answer
+        const question = this.state.question
+            return (
+                <div className="Question">
+
+                    <div className="Question_question">
+                        <form>
+
+                            <p> {question}</p>
+
+                            <button className=" answerButton" onClick={this.submitAnswer}>{answer}</button>
+                            <button className=" answerButton" onClick={this.submitAnswer}>{answer}</button>
+                            <button className=" answerButton" onClick={this.submitAnswer}>{answer}</button>
+                            <button className=" answerButton" onClick={this.submitAnswer}>{answer}</button>
+                        </form>
+
+                    </div>
+                </div>
+            );
+        }
+    }
+
 
 
 export default Question;
