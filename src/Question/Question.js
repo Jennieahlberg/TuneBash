c8f41bb33ef2cd88ed2cc5403ac77d20a71ae3bb
katarinasandart <katarina.sandart@hotmail.com>
2018-09-06 09:48:23 +0200

    Refactor Spotify button

--- src/Question/Question.js
+++ src/Question/Question.js
@@ -3,7 +3,15 @@ import React from "react";
 const Question = props => {
     return(
         <div className="Question">
-        
+        <iframe
+          src="https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3"
+          width="400"
+          height="80"
+          frameborder="0"
+          allowtransparency="true"
+          allow="encrypted-media"
+          title="music"
+        />
         <div id="Question_question">
           <div className="Product__cat">{question.varugrupp}</div>
           <h4>{product.namn}</h4>
