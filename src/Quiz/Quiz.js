3b728615d8456db8c4501e81f5c35a944d836f2b
katarinasandart <katarina.sandart@hotmail.com>
2018-09-11 09:33:16 +0200

    Same question for everyone and finish

--- src/Quiz/Quiz.js
+++ src/Quiz/Quiz.js
@@ -2,23 +2,48 @@ import React, { Component } from "react";
 import "./Quiz.css";
 import Question from "../Question/Question";
 import GameResults from "../GameResults/GameResults";
+import io from "socket.io-client";
 
+const socketUrl = "http://localhost:3231";
 class Quiz extends Component {
   constructor(props) {
     super(props);
     this.state = { 
       counter: 0,
       usersArray: this.props.usersArray,
-      gameEnded: false
+      gameEnded: false,
+      socket: io(socketUrl), 
     };
 
     this.nextQuestion = this.nextQuestion.bind(this);
     console.log(this.state.usersArray);
   }
 
-  nextQuestion = () => {
-    this.setState({ counter: this.state.counter + 1 });
+   
+  componentWillMount() {
+    this.onSocket();
+    this.next();
+  }
+
+  onSocket = () => {
+    this.state.socket.on("user joined", data => {
+      console.log(data);
+      this.setState({ usersArray: data.users });
+    });
   };
+
+  nextQuestion() {
+    this.setState({ counter: this.state.counter + 1 });
+    this.state.socket.emit('next', this.state.counter + 1);
+  };
+
+  
+  next() {
+    this.state.socket.on('next', (nextquestion) => {
+      this.setState({counter: nextquestion });
+    })
+    console.log("startgame " + this.state.start);
+  }
 
   endGame = () => {
     this.setState({ gameEnded: true });
@@ -29,11 +54,11 @@ class Quiz extends Component {
     console.log(quizz);
     console.log(this.props.questions);
 
-    if(this.gameEnded){
+    if(this.state.counter >= quizz.length){
         return(
         <GameResults results={this.props.results}/>)
-
     }
+
     return (
       <div className="questions">
         <Question
