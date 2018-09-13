42e2c24dde8a78d8c47c50458e28bd23bbb95ce2
Katarina Käll <katarina_kall86@hotmail.com>
2018-09-13 09:59:33 +0200

    Added method to render questions and correct answer after game ended

--- src/GameResults/GameResults.js
+++ src/GameResults/GameResults.js
@@ -3,36 +3,33 @@ import React, { Component } from "react";
 class GameResults extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            resultBoard: [
-              ["kurt", 10, "bla", 10, "bla"],
-              ["lasse", 13, "la", 1, "blala"],
-              ["gunilla", 18, "blananan", 10, "bla"],
-              ["barbro", 12, "nanana", 1, "bla"],
-            ]
-          };
+
     }
 
 
   sortResults = () => {
-    this.state.resultBoard.sort((a, b) => {
+    this.props.usersArray.sort((a, b) => {
         return b[1] - a[1];
     })
   };
 
   render() {
     this.sortResults();
-    console.log(this.state.correctAnswers);
     console.log(this.props.usersArray);
     console.log(this.props.questions);
     return (
       <div>
-          <h1>Vinnare: {this.props.usersArray[0][0]} med {this.props.usersArray[0][1]} poäng!</h1>
+          {/*<h1>Vinnare:{this.props.usersArray[0][0]} med {this.props.usersArray[0][1]} poäng!</h1>*/}
 
           <p> Resultat:</p>
-          {/*<h1>Vinnare: {this.state.resultBoard[0][0]} {this.state.resultBoard[0][1]}</h1>*/}
-        {this.state.resultBoard.map((result) => {
-           return(<p>{result[0]} {result[1]}</p>)})}
+        {this.props.usersArray.map((result) => {
+           return(<p>{result[0]} {result[1]} poäng</p>)})}
+
+           <p></p>
+          {this.props.questions.map((questionsAndAnwers) =>{
+              return <p>{questionsAndAnwers.question} Rätt svar: {questionsAndAnwers.correctAnswer}</p>
+              }
+          )}
       </div>
     );
   }
