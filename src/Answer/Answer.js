e999d192a30912501dcb27eab6d61fc8094ef962
katarinasandart <katarina.sandart@hotmail.com>
2018-09-14 09:41:28 +0200

    Disabling buttons

--- src/Answer/Answer.js
+++ src/Answer/Answer.js
@@ -12,23 +12,32 @@ class Answer extends Component {
       value: "",
       shuffle: true,
       end: false,
-      disableAnswers: false
+      disableButton: this.props.disableButton
     };
     this.submitAnswer = this.submitAnswer.bind(this);
   }
 
   componentWillMount() {
     this.gameEnded();
+    this.next();
+  }
+
+  next() {
+    this.state.socket.on("next", nextquestion => {
+      this.setState({disableButton: false});
+    });
+    console.log("hej");
   }
 
   submitAnswer(event) {
+    this.setState({disableButton: true})
     event.preventDefault();
     const question = this.props.question;
     this.state.usersArray.push(event.target.value);
     if (event.target.value === question.correctAnswer) {
       this.state.usersArray[1]++;
     }
-    this.setState({disableAnswers: true});
+    const targetValue = event.target.value;
   }
 
   gameEnded() {
@@ -40,43 +49,45 @@ class Answer extends Component {
   }
 
   render() {
-    const question = this.props.question;
-    const disableAnswers = this.state.disableAnswers;
     const answers = this.props.answers;
+  
     console.log(answers);
+    console.log(this.props.disableButton);
+    console.log(this.state.disableButton);
+    console.log(this.state.usersArray);
 
     return (
       <div className="buttonFormDiv">
         <form className="buttonForm">
           <button
-            className="answerButton"
+            className="answerButton" 
             value={answers[0]}
             onClick={event => this.submitAnswer(event)}
-            {...disableAnswers ? <disabled/> : <normal/> }
+            disabled={this.state.disableButton}
           >
             {answers[0]}
           </button>
           <button
-            className="answerButton"
+            className="answerButton" 
             value={answers[1]}
             onClick={event => this.submitAnswer(event)}
-            {...disableAnswers ? <disabled/> : <normal/> }
+            disabled={this.state.disableButton}
           >
             {answers[1]}
           </button>
           <button
-            className="answerButton"
+            className="answerButton" 
             value={answers[2]}
             onClick={event => this.submitAnswer(event)}
-            {...disableAnswers ? <disabled/> : <normal/> }
+            disabled={this.state.disableButton}
           >
             {answers[2]}
           </button>
           <button
-            className="answerButton"
+            className="answerButton" 
             value={answers[3]}
             onClick={event => this.submitAnswer(event)}
-            {...disableAnswers ? <disabled/> : <normal/> }
+            disabled={this.state.disableButton}
           >
             {answers[3]}
           </button>
